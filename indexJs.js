3c7b40c8755a5afe51d36626db4d5469820748fd
xXxbElKaxXx <104276565+xXxbElKaxXx@users.noreply.github.com>
2022-06-14 21:08:17 +0300

    Update indexJs.js

--- indexJs.js
+++ indexJs.js
@@ -14,7 +14,10 @@ buttonSettings.onclick = function() {
   settings.style.display = 'block';
   audioSettings.play();
 }
-settingsCross.onclick = () => settings.style.display = 'none';
+settingsCross.onclick = () => {
+  settings.style.display = 'none';
+  event.stopPropagation();
+}
 
 let audioClick = document.getElementById('audioClick');
 audioClick.volume = 0.3;
@@ -44,14 +47,6 @@ let countScore = 0;
 let allCombos = [];
 let countCombo = 0;
 
-let arraySetSpeed = document.getElementsByName('setSpeed');
-let arraySetTimer = document.getElementsByName('setTimer');
-function getCheckedRadio(array) {
-  for (let radio of array) {
-    if (radio.checked) return radio.value
-  }
-}
-
 document.oncontextmenu = function() {
   return false;
 };
@@ -64,23 +59,38 @@ function getMaxOfArray(numArray) {
   return Math.max.apply(null, numArray);
 }
 
+let arraySetSpeed = document.getElementsByName('setSpeed');
+let arraySetTimer = document.getElementsByName('setTimer');
+
+function getCheckedRadio(array) {
+  for (let radio of array) {
+    if (radio.checked) return radio.value
+  }
+}
+
 function startTimer() {
   seconds = getCheckedRadio(arraySetTimer);
   window.timerIdTimer = setInterval(() => {
-    seconds = seconds - 1;
+    seconds--;
     timer.value = `timer: ${seconds}`;
   }, 1000);
 }
 
+function closeOtherDisplays() {
+  rules.style.display = 'none';
+  settings.style.display = 'none';
+  finalAlert.style.display = 'none';
+}
+
 function startGame() {
   audioStart.play();
-  settings.style.display = 'none';
-  rules.style.display = 'none';
-  finalAlert.style.display = 'none';
+  closeOtherDisplays();
   countScore = 0;
   countCombo = 0;
   allCombos.length = 0;
   stopGame();
+  screen.addEventListener("mousedown", countClicker);
+  document.addEventListener("mouseover", countKeyClicker);
   startTimer();
   window.timerIdGame = setInterval(() => {
     showAim();
@@ -93,14 +103,125 @@ function startGame() {
 }
 
 function stopGame() {
+  screen.removeEventListener("mousedown", countClicker);
+  document.removeEventListener("mouseover", countKeyClicker);
+  document.onkeydown = null;
   timer.value = `timer:`;
-  let aims = document.querySelectorAll('.aim');
+  let aims = screen.querySelectorAll('.aim');
   for (aim of aims) {
     aim.remove()
   }
   clearInterval(window.timerIdGame);
   clearInterval(window.timerIdTimer);
-  screen.onmousedown = null;
+}
+
+function countClicker(event) {
+  if (event.which == 1 && event.target.style.backgroundImage == 'url("aim1.jpg")') {
+    countScore = countScore + 1;
+    countCombo++;
+    score.value = `combo: ${countCombo} | score: ${countScore}`;
+    audioClick.play();
+    event.target.remove();
+  } else if (event.which == 1 && event.target.style.backgroundImage == 'url("aim3.jpg")') {
+    allCombos.push(countCombo);
+    countScore--;
+    countCombo = 0;
+    score.value = `combo: ${countCombo} | score: ${countScore}`;
+    audioMiss.play();
+  } else if (event.which == 3 && event.target.style.backgroundImage == 'url("aim3.jpg")') {
+    countScore++;
+    countCombo++;
+    score.value = `combo: ${countCombo} | score: ${countScore}`;
+    audioClick.play();
+    event.target.remove();
+  } else if (event.which == 3 && event.target.style.backgroundImage == 'url("aim1.jpg")') {
+    allCombos.push(countCombo);
+    countScore--;
+    countCombo = 0;
+    score.value = `combo: ${countCombo} | score: ${countScore}`;
+    audioMiss.play();
+  } else {
+    allCombos.push(countCombo);
+    countScore--;
+    countCombo = 0;
+    score.value = `combo: ${countCombo} | score: ${countScore}`;
+    audioMiss.play();
+  }
+}
+
+function countKeyClicker(event) {
+  if (event.target.style.backgroundImage == 'url("aim1.jpg")') {
+    document.onkeydown = function(e) {
+      if (e.code == 'KeyX') {
+        countScore++;
+        countCombo++;
+        score.value = `combo: ${countCombo} | score: ${countScore}`;
+        audioClick.play();
+        event.target.remove();
+      }
+      else if (e.code == 'KeyZ') {
+        allCombos.push(countCombo);
+        countScore--;
+        countCombo = 0;
+        score.value = `combo: ${countCombo} | score: ${countScore}`;
+        audioMiss.play();
+      }
+    }
+  } else if (event.target.style.backgroundImage == 'url("aim3.jpg")') {
+    document.onkeydown = function(e) {
+      if (e.code == 'KeyZ') {
+        countScore++;
+        countCombo++;
+        score.value = `combo: ${countCombo} | score: ${countScore}`;
+        audioClick.play();
+        event.target.remove();
+      }
+      else if (e.code == 'KeyX') {
+        allCombos.push(countCombo);
+        countScore--;
+        countCombo = 0;
+        score.value = `combo: ${countCombo} | score: ${countScore}`;
+        audioMiss.play();
+      }
+    }
+  } else if (event.target == screen) {
+    document.onkeydown = function(e) {
+      if (e.code == 'KeyX' || e.code == 'KeyZ') {
+        allCombos.push(countCombo);
+        countScore--;
+        countCombo = 0;
+        score.value = `combo: ${countCombo} | score: ${countScore}`;
+        audioMiss.play();
+      }
+    }
+  } else if (event.relatedTarget == screen) {
+    document.onkeydown = null
+  }
+}
+
+document.onmouseout = function(event) {
+  for (let button of document.getElementsByTagName('button')) {
+    if (event.relatedTarget == button) {
+      document.onkeydown = function(e) {
+        if (e.code == 'KeyX') {
+          event.relatedTarget.onclick()
+        }
+      }
+    } else if (event.target == button) {
+      document.onkeydown = null
+    }
+  }
+  for (let span of document.getElementsByTagName('span')) {
+    if (event.relatedTarget == span) {
+      document.onkeydown = function(e) {
+        if (e.code == 'KeyX') {
+          event.relatedTarget.onclick()
+        }
+      }
+    } else if (event.target == span) {
+      document.onkeydown = null
+    }
+  }
 }
 
 function showAim() {
@@ -119,48 +240,19 @@ function showAim() {
 
   screen.append(aim);
 
-  screen.onmousedown = function(event) {
-    if (event.which == 1 && event.target.style.backgroundImage == 'url("aim1.jpg")') {
-      countScore++;
-      countCombo++;
-      score.value = `combo: ${countCombo} | score: ${countScore}`;
-      audioClick.play();
-      event.target.remove();
-    } else if (event.which == 1 && event.target.style.backgroundImage == 'url("aim3.jpg")') {
-      allCombos.push(countCombo);
-      countScore--;
-      countCombo = 0;
-      score.value = `combo: ${countCombo} | score: ${countScore}`;
-      audioMiss.play();
-    } else if (event.which == 3 && event.target.style.backgroundImage == 'url("aim3.jpg")') {
-      countScore++;
-      countCombo++;
-      score.value = `combo: ${countCombo} | score: ${countScore}`;
-      audioClick.play();
-      event.target.remove();
-    } else if (event.which == 3 && event.target.style.backgroundImage == 'url("aim1.jpg")') {
-      allCombos.push(countCombo);
-      countScore--;
-      countCombo = 0;
-      score.value = `combo: ${countCombo} | score: ${countScore}`;
-      audioMiss.play();
-    } else {
-      allCombos.push(countCombo);
-      countScore--;
-      countCombo = 0;
-      score.value = `combo: ${countCombo} | score: ${countScore}`;
-      audioMiss.play();
-    }
-  }
-  setTimeout(() => aim.remove(), 3000);
+  setTimeout( () => aim.remove() , 3000);
 }
 
 let finalAlert = document.querySelector('.finalAlert');
 let buttonFinalAlert = document.getElementById('buttonFinalAlert');
+let alertTimer = document.getElementById('alertTimer');
+let alertDifficult = document.getElementById('alertDifficult');
 let alertScore = document.getElementById('alertScore');
+let alertCombo = document.getElementById('alertCombo');
 buttonFinalAlert.onclick = () => finalAlert.style.display = 'none';
 function showFinalAlert() {
   finalAlert.style.display = 'block';
+  alertTimer.textContent = `${getCheckedRadio(arraySetTimer)} sec`;
   alertScore.textContent = `${countScore}`;
   alertCombo.textContent = `${getMaxOfArray(allCombos)}`;
   if (getCheckedRadio(arraySetSpeed) == 800) {
@@ -169,10 +261,10 @@ function showFinalAlert() {
   if (getCheckedRadio(arraySetSpeed) == 500) {
     alertDifficult.textContent = `normal`;
   }
-  if (getCheckedRadio(arraySetSpeed) == 350) {
+  if (getCheckedRadio(arraySetSpeed) == 400) {
     alertDifficult.textContent = `hard`;
   }
-  if (getCheckedRadio(arraySetSpeed) == 200) {
+  if (getCheckedRadio(arraySetSpeed) == 300) {
     alertDifficult.textContent = `nightmer`;
   }
 }
